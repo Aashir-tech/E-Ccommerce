8724076ca04ba6282f39b7c093d4d82e50cc7cf0
Aashir-tech <118300747+Aashir-tech@users.noreply.github.com>
2024-09-18 19:54:55 +0530

    Fixed again

--- backend/index.js
+++ backend/index.js
@@ -32,19 +32,19 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
 
-// Handling Unhandled Routes
-app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find ${req.originalUrl} on this server !`,
-  });
-});
+// // Handling Unhandled Routes
+// app.all("*", (req, res, next) => {
+//   res.status(404).json({
+//     status: "fail",
+//     message: `Can't find ${req.originalUrl} on this server !`,
+//   });
+// });
 
 // MiddleWare for errors
 
