18dbfc029f39912651d5bdcc040f196e0f4acf53
Aashir-tech <118300747+Aashir-tech@users.noreply.github.com>
2024-09-18 19:52:43 +0530

    Fixed some error

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
-// const path = require("path");
+const path = require("path");
 
 require("dotenv").config({
   path : "./.env"
@@ -32,11 +32,11 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-// app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 // Handling Unhandled Routes
 app.all("*", (req, res, next) => {
