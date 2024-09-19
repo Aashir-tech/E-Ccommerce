ae45caa4b563b93208ad5f3eba23076c96d19fef
Aashir-tech <118300747+Aashir-tech@users.noreply.github.com>
2024-09-19 16:01:18 +0530

    Replace ReactDOM.createRoot with ReactDOM.render

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,27 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { positions, transitions, Provider as AlertProvider } from "react-alert";
-
 import AlertTemplate from "react-alert-template-basic";
 
-
-
 const options = {
-  timeout : 5000,
-  position : positions.BOTTOM_CENTER,
-  transition : transitions.SCALE
-}
+  timeout: 5000,
+  position: positions.BOTTOM_CENTER,
+  transition: transitions.SCALE
+};
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <AlertProvider template={AlertTemplate} {...options}>
         <App />
       </AlertProvider>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
