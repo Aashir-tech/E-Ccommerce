68e9b630470446573ae75a0df283bd3afaccee34
Aashir-tech <118300747+Aashir-tech@users.noreply.github.com>
2024-09-24 12:00:06 +0530

    Authentication issue

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,4 +1,5 @@
 // Creating Token and Saving in Cookie
+const isProduction = process.env.NODE_ENV === 'production';
 
 const sendToken = (user, statusCode, res) => {
   const token = user.getJWTToken();
@@ -9,8 +10,10 @@ const sendToken = (user, statusCode, res) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,  // accessible only by web server
-    secure: process.env.NODE_ENV === "production",  // cookie is sent only over HTTPS
-    sameSite : process.env.NODE_ENV === "production" ? "None" : "Lax",  // required for cross-site cookies
+    secure: isProduction,  // true in production (only over HTTPS)
+    sameSite: isProduction ? 'None' : 'Lax',  // 'None' for cross-origin in production
+    // secure: process.env.NODE_ENV === "production",  // cookie is sent only over HTTPS
+    // sameSite : process.env.NODE_ENV === "production" ? "None" : "Lax",  // required for cross-site cookies
   };
 
   res.status(statusCode).cookie("token", token, options).json({
